97fc2e086c841f35bd08e936e80d1c9a1dac1089
dbosquez <48219800+dbosquez@users.noreply.github.com>
2019-04-20 15:56:56 -0500

    Solution for all U's except when x =0
    Also made convergence study 3D plots

--- SCProject_GausSeid.m
+++ SCProject_GausSeid.m
@@ -1,13 +1,14 @@
 %% Scientific Computing Project 2D Poisson Eqn.(AP02-2),   Daniel Bosquez
 % Gauss Seidel:
+clc
+close all
 
-close
-clear
+
 
 % Define grid
 
-iter = 5; %iterations for convergence
-N = 10; %grid intervals
+iter = 500; %iterations for convergence
+N = 1000; %grid intervals
 h = (2*pi)/(N+1); %grid step dx = dy
 ax = 0;
 ay = ax;
@@ -21,7 +22,8 @@ j = st';
 k = st;
 %j=2;
 %k=2;
-
+xval=(h.*j-1);
+yval=(h.*k-1);
 % Initialize and vectorize knowns
 
 F = sin(pi.*(((h.*(j-1))-ax)./(bx-ax)))*cos((pi/2).*((2.*(((h.*(k-1))-ay)./(by-ay)))+1)); % F matrix of known F's for all x and y's
@@ -38,8 +40,8 @@ U(len,:) = ga(len)+((((h.*(k-1))-ay)/(bx-ay))*(fa(len)-ga(len))); % U(bx,y) BC
 U2 = U(:); % vectorized array
 %U=U(:);
 % Commence Gauss Seidel Vector solver
-%%
-for i=1:1000
+
+for i=1:iter
  for K = 2:len-1
      for J = 2:len-1
          U(J,K)= (.25*(U(J-1,K)+U(J+1,K)+U(J,K-1)+U(J,K+1)))+(.25*h*h*F(J+((K-1)*len)));
@@ -48,5 +50,18 @@ for i=1:1000
  end
 
 end
-U1000 = U;
+
+figure;
+contour3(xval,yval,U,len,'ShowText','off')
+xlabel('0 < X < 2pi')
+ylabel('0 < Y < 2pi')
+zlabel('U')
+
+
+figure;
+contour3(144,144,U144,len,'ShowText','off')
+xlabel('0 < X < 2pi')
+ylabel('0 < Y < 2pi')
+zlabel('U')
+
 
