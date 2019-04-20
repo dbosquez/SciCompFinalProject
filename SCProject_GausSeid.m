8a8efa36a5c1ab42d7d6467c1cc250f36df1bbdd
dbosquez <48219800+dbosquez@users.noreply.github.com>
2019-04-19 21:15:37 -0500

    Vectorized knowns
    F, fa, ga, and basic grid steps

--- SCProject_GausSeid.m
+++ SCProject_GausSeid.m
@@ -9,12 +9,25 @@ clear
 iter = 5; %iterations for convergence
 N = 10; %grid intervals
 h = (2*pi)/(N+1); %grid step dx = dy
+ax = 0;
+ay = ax;
+bx = 2*pi;
+by = bx;
+st = [1:N+2] % number of x and y steps
+j = st';
+k = st;
+%j=2;
+%k=2;
 
-x = [1:N+1] % number of x steps
-y = x;      % number of y steps
+% Initialize and vectorize knowns
 
+F = sin(pi.*(((h.*(j-1))-ax)./(bx-ax)))*cos((pi/2).*((2.*(((h.*(k-1))-ay)./(by-ay)))+1)); % F matrix of known F's for all x and y's
+F = F(:); % Vectorizes F matrix
+fa = (h.*(j-1)).*((h.*(j-1))-ax).^2;
+ga = ((h.*(j-1))-ax).^2.*cos((h.*(j-1)));
 
-
+% Next up I need to create the U vector then populate with known
+% conditions.
 
 
 
